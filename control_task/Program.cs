cba67bf0572964b3cf0976ab64ed838e39e8bfd9
Dante67yt <dante67yt@gmail.com>
2022-08-14 14:08:51 +0300

    create first matrix with random numbers [1, 2000]

---
+++ control_task/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше,
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения
+// алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 2000);
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+CreateMatrix(m, n);
